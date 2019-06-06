3ed1b35e2dc9946d6da23954317fc61b58a7d57e
guanyun66 <1547727838@qq.com>
2019-06-06 18:25:27 +0800

    第一次作业

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -57,21 +57,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //for循环
   //定义一个空值，然后for循环找出在无序区域中最小的一个，然后再用最小的跟无序区域的第一个数值的位置交换。
   //一直重复以上的循环，直到[i]=[j];
-        int R0=0;
-        for (int i=1;i<items.length;i++){
-            for (int k=0;k<items.length;k++){
-                for (int j=0;j<=i;j++){
-                   if (items[i]>items[i+1]){
-                       R0=items[i+1];
-                       items[i+1]=items[i];
-                       items[i]=R0;
-                       break;
-                    }
-
+        for (int i=0;i<items.length;i++){
+            for (int j=i+1;j<items.length;j++){
+                if (items[i]>items[j]){
+                    int R=items[i];
+                    items[i]=items[j];
+                    items[j]=R;
                 }
             }
         }
 
+
+
+//        int R0=0;
+//        for (int i=0;i<items.length;i++){
+//                for (int j=i+1;j<=i;j++){
+//                   if (items[i]>items[j]){
+//                       R0=items[j];
+//                       items[j]=items[i];
+//                       items[i]=R0;
+//                    }
+//                }
+//            }
     }
 
     private void generateItems() {
